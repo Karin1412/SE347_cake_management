1e1d7d6c4c390956136938aaa25fa6079b358e43
PhucDanh1412 <21521924@gm.uit.edu.vn>
2023-12-19 14:02:47 +0700

    Add cart

--- src/index.js
+++ src/index.js
@@ -7,18 +7,21 @@ import Signup from './auth/signup';
 import ProductDetail from './views/productDetail';
 import AboutUs from './views/aboutUs'
 import Home from './views/home'
-
-
+import Cart from './views/cartview'
+import NavBar from './components/productDetails/navbar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>}></Route>
         <Route path="/about-us" element={<AboutUs/>}></Route>
         <Route path="/home" element={<Home/>}></Route>
         <Route path="/sign-up" element={<Signup/>}></Route>
+        <Route path="/cart" element={<Cart/>}></Route>
         <Route path="/product-detail" element={<ProductDetail/>}></Route>
+        <Route path='/nav-bar' element={<NavBar/>}></Route>
       </Routes>
     </BrowserRouter>
     
