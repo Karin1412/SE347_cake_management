4502925940b6c493e00f7ba9ddf88fbae3081d3d
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-01-19 17:29:16 +0700

    update auth

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,85 +1,128 @@
-import React from 'react';
-import BackLog from '../assets/back_log.avif'
-import { Link } from 'react-router-dom';
-import FadeIn from '../components/animations/Fadein';
-
-
+import React, { useEffect } from "react";
+import BackLog from "../assets/back_log.avif";
+import { Link } from "react-router-dom";
+import FadeIn from "../components/animations/Fadein";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as UserService from "../services/UserService";
+import { useMutationHooks } from "../hooks/useMutationHook";
+import { useNavigate, useLocation } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    /*
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleLogin = async (e) => {
-    e.preventDefault();
-
-    try {
-      // Tạo một tài khoản người dùng mới
-      const { user } = await appwrite.account.createSession(email, password);
-
-      console.log('Logged in:', user);
-      <Link to ="/home"></Link>
-      // Xử lý sau khi đăng nhập thành công, ví dụ: điều hướng đến trang chính
-    } catch (error) {
-      console.error('Login failed:', error);
-      // Xử lý lỗi đăng nhập
+  const mutation = useMutationHooks((data) => UserService.loginUser(data));
+  const { data, isLoading, isSuccess } = mutation;
+  useEffect(() => {
+    if (isSuccess) {
+      if (location?.state) {
+        navigate(location?.state);
+      } else {
+        navigate("/home");
+      }
+      /*  localStorage.setItem('access_token', JSON.stringify(data?.access_token))
+          localStorage.setItem('refresh_token', JSON.stringify(data?.refresh_token)) */
+      //if (data?.access_token) {
+      //const decoded = jwt_decode(data?.access_token)
+      /*  if (decoded?.id) {
+              handleGetDetailsUser(decoded?.id, data?.access_token)
+            } 
+          }*/
     }
+  }, [isSuccess]);
+  /*  const handleGetDetailsUser = async (id, token) => {
+        const storage = localStorage.getItem('refresh_token')
+        const refreshToken = JSON.parse(storage)
+        const res = await UserService.getDetailsUser(id, token)
+
+      } */
+  const handleOnChangeEmail = (event) => {
+    setEmail(event.target.value);
   };
 
-*/
-    return (
-        <FadeIn delay={0.2} direction="left">
-            <div className='bg-gray-100 h-screen flex items-center justify-center'>
-            <div className="max-w-[980px] w-full bg-white p-8 rounded-md shadow-md flex">
-                <div className="w-1/4">
-                    <img src={BackLog} alt="" />
-                </div>
+  const handleOnchangePassword = (event) => {
+    setPassword(event.target.value);
+  };
 
-            <div className="w-3/4 px-8 flex flex-col">
-                <h3 className="text-2xl font-semibold mb-4 self-start text-[#69432B]">Chocobrat</h3>
-                <p className='mt-5 ml-16'>Welcome back, <p className='flex'>Let's go to <p className='text-[#69432B] font-bold ml-1'> Chocobrat</p></p> </p>
-                <form action="#" method="post">
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="mt-5 ml-16 p-2 border border-solid border-#69432B rounded-md text-[#69432B] placeholder-[#69432B] w-full md:w-1/2 focus:outline-none focus:border-orange-950 focus:bg-orange-50"
-                        placeholder="Email"
-                    />
-                    <div className="mb-6">
-              
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        className="mt-5 ml-16 p-2 border border-solid border-#69432B rounded-md text-[#69432B] placeholder-[#69432B] w-full md:w-1/2 focus:outline-none focus:bg-orange-50 focus:border-orange-950"
-                        placeholder="Password"
-                        
-                    />
-                    </div>
-                    <Link to={'/home'}>
-                        <button
-                            type="submit"
-                            className="bg-[#69432B] text-white py-2 px-4 ml-16 mt-1 rounded-md hover:bg-orange-950 focus:outline-none focus:ring focus:border-blue-300 md:w-1/2"
-                        >
-                            Login
-                        </button>
-                    </Link> 
-                    
-                 
-                   
+  const handleSignIn = () => {
+    console.log("logingloin");
+    mutation.mutate({
+      email,
+      password,
+    });
+  };
 
-                    <p className="mt-4 ml-16 flex">
-                        If you don't have an account, <Link to={'/sign-up'}><p className='text-[#69432B] font-bold ml-1'>register</p></Link>
-                    </p>
-                </form>
+  return (
+    <FadeIn delay={0.2} direction="left">
+      <div className="bg-gray-100 h-screen flex items-center justify-center">
+        <div className="max-w-[980px] w-full bg-white p-8 rounded-md shadow-md flex">
+          <div className="w-1/4">
+            <img src={BackLog} alt="" />
+          </div>
+
+          <div className="w-3/4 px-8 flex flex-col">
+            <h3 className="text-2xl font-semibold mb-4 self-start text-[#69432B]">
+              Chocobrat
+            </h3>
+            <p className="mt-5 ml-16">
+              Welcome back,{" "}
+              <span className="flex">
+                Let's go to{" "}
+                <span className="text-[#69432B] font-bold ml-1">
+                  {" "}
+                  Chocobrat
+                </span>
+              </span>{" "}
+            </p>
+            <form action="#" method="post">
+              <input
+                type="email"
+                //id="email"
+                //name="email"
+                className="mt-5 ml-16 p-2 border border-solid border-#69432B rounded-md text-[#69432B] placeholder-[#69432B] w-full md:w-1/2 focus:outline-none focus:border-orange-950 focus:bg-orange-50"
+                placeholder="Email"
+                value={email}
+                onChange={handleOnChangeEmail}
+              />
+              <div className="mb-6">
+                <input
+                  type="password"
+                  //id="password"
+                  //name="password"
+                  className="mt-5 ml-16 p-2 border border-solid border-#69432B rounded-md text-[#69432B] placeholder-[#69432B] w-full md:w-1/2 focus:outline-none focus:bg-orange-50 focus:border-orange-950"
+                  placeholder="Password"
+                  value={password}
+                  onChange={handleOnchangePassword}
+                />
+              </div>
+              <Link to={"/home"}>
+                <button
+                  type="submit"
+                  onClick={handleSignIn}
+                  className="bg-[#69432B] text-white py-2 px-4 ml-16 mt-1 rounded-md hover:bg-orange-950 focus:outline-none focus:ring focus:border-blue-300 md:w-1/2"
+                >
+                  Login
+                </button>
+              </Link>
+
+              <p className="mt-4 ml-16 flex">
+                If you don't have an account,{" "}
+                <Link to={"/sign-up"}>
+                  <p className="text-[#69432B] font-bold ml-1">register</p>
+                </Link>
+              </p>
+            </form>
+          </div>
         </div>
       </div>
-        </div>
-        </FadeIn>
-        
-    );
+    </FadeIn>
+  );
 };
 
 export default Login;
--- src/auth/signup.js
+++ src/auth/signup.js
@@ -1,70 +1,114 @@
-import React from 'react';
-import BgSignUp from '../assets/bg_signup.avif'
-import { Link } from 'react-router-dom';
-import FadeIn from '../components/animations/Fadein';
+import React from "react";
+import BgSignUp from "../assets/bg_signup.avif";
+import { Link, NavLink } from "react-router-dom";
+import FadeIn from "../components/animations/Fadein";
+import { useState } from "react";
+import { Image } from "antd";
+import { useMutationHooks } from "../hooks/useMutationHook";
+import * as UserService from "../services/UserService";
+import * as message from "../components/message/message";
+import { useEffect } from "react";
 
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
+  const handleOnchangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
 
-const Login = () => {
+  const handleOnchangePassword = (event) => {
+    setPassword(event.target.value);
+  };
 
-    return (
-        <FadeIn delay={0.2} direction="right">
-            <div className='bg-gray-100 h-screen flex items-center justify-center'>
-            <div className="max-w-[980px] w-full bg-white p-8 rounded-md shadow-md flex">
-                <div className="w-3/4 px-8 flex flex-col">
-                    <h3 className="text-2xl font-semibold mb-4 self-start text-[#69432B]">Chocobrat</h3>
-                    <p className='mt-5 ml-16'>Hello, <p className='flex'>Let's start with <p className='text-[#69432B] font-bold ml-1'> Chocobrat</p></p> </p>
-                    <form action="#" method="post">
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            className="mt-5 ml-16 p-2 border border-solid border-#69432B rounded-md text-[#69432B] placeholder-[#69432B] w-full md:w-1/2 focus:outline-none focus:border-orange-950 focus:bg-orange-50"
-                            placeholder="Email"
-                        />
-                        <div className="mb-6">
-                
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            className="mt-5 ml-16 p-2 border border-solid border-#69432B rounded-md text-[#69432B] placeholder-[#69432B] w-full md:w-1/2 focus:outline-none focus:bg-orange-50 focus:border-orange-950"
-                            placeholder="Password"
-                            
-                        />
+  const handleOnchangeConfirmPassword = (event) => {
+    setConfirmPassword(event.target.value);
+  };
 
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            className="mt-5 ml-16 p-2 border border-solid border-#69432B rounded-md text-[#69432B] placeholder-[#69432B] w-full md:w-1/2 focus:outline-none focus:bg-orange-50 focus:border-orange-950"
-                            placeholder="Confirm Password"
-                            
-                        />
-                        </div>
-                        <button
-                            type="submit"
-                            className="bg-[#69432B] text-white py-2 px-4 ml-16 mt-1 rounded-md hover:bg-orange-950 focus:outline-none focus:ring focus:border-blue-300 md:w-1/2"
-                        >
-                            Sign up
-                        </button>
-                        
-                    
-                    
+  const mutation = useMutationHooks((data) => UserService.signupUser(data));
 
-                        <p className="mt-4 ml-16 flex">
-                            Already have an account, <Link to={'/'}><p className='text-[#69432B] font-bold ml-1'>sign in</p></Link>
-                        </p>
-                    </form>
-                </div>
-                <div className="w-1/4">
-                    <img src={BgSignUp} alt="" />
-                </div>
-            </div>
+  const { data, isLoading, isSuccess, isError } = mutation;
+
+  useEffect(() => {
+    if (isSuccess) {
+      message.success();
+    } else if (isError) {
+      message.error();
+    }
+  }, [isSuccess, isError]);
+
+  const handleSignUp = () => {
+    mutation.mutate({ email, password, confirmPassword });
+  };
+  return (
+    <FadeIn delay={0.2} direction="right">
+      <div className="bg-gray-100 h-screen flex items-center justify-center">
+        <div className="max-w-[980px] w-full bg-white p-8 rounded-md shadow-md flex">
+          <div className="w-3/4 px-8 flex flex-col">
+            <h3 className="text-2xl font-semibold mb-4 self-start text-[#69432B]">
+              Chocobrat
+            </h3>
+            <p className="mt-5 ml-16">
+              Hello,{" "}
+              <p className="flex">
+                Let's start with{" "}
+                <p className="text-[#69432B] font-bold ml-1"> Chocobrat</p>
+              </p>{" "}
+            </p>
+            <form action="#" method="post">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="mt-5 ml-16 p-2 border border-solid border-#69432B rounded-md text-[#69432B] placeholder-[#69432B] w-full md:w-1/2 focus:outline-none focus:border-orange-950 focus:bg-orange-50"
+                placeholder="Email"
+                value={email}
+                onChange={handleOnchangeEmail}
+              />
+              <div className="mb-6">
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="mt-5 ml-16 p-2 border border-solid border-#69432B rounded-md text-[#69432B] placeholder-[#69432B] w-full md:w-1/2 focus:outline-none focus:bg-orange-50 focus:border-orange-950"
+                  placeholder="Password"
+                  value={password}
+                  onChange={handleOnchangePassword}
+                />
+
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="mt-5 ml-16 p-2 border border-solid border-#69432B rounded-md text-[#69432B] placeholder-[#69432B] w-full md:w-1/2 focus:outline-none focus:bg-orange-50 focus:border-orange-950"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={handleOnchangeConfirmPassword}
+                />
+              </div>
+              <button
+                onClick={handleSignUp}
+                className="bg-[#69432B] text-white py-2 px-4 ml-16 mt-1 rounded-md hover:bg-orange-950 focus:outline-none focus:ring focus:border-blue-300 md:w-1/2"
+              >
+                Sign up
+              </button>
+
+              <p className="mt-4 ml-16 flex">
+                Already have an account,{" "}
+                <Link to={"/"}>
+                  <p className="text-[#69432B] font-bold ml-1">sign in</p>
+                </Link>
+              </p>
+            </form>
+          </div>
+          <div className="w-1/4">
+            <img src={BgSignUp} alt="" />
+          </div>
         </div>
-        </FadeIn>
-        
-    );
+      </div>
+    </FadeIn>
+  );
 };
 
-export default Login;
+export default SignUp;
